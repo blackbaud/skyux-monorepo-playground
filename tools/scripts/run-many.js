7d3dee27e5bd81721c0ad881008adfefd637eb74
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2022-01-27 09:10:57 -0500

    Add console warns

--- tools/scripts/run-many.js
+++ tools/scripts/run-many.js
@@ -1,35 +1,39 @@
 const execSync = require('child_process').execSync;
 
-const target = process.argv[2];
-const jobIndex = Number(process.argv[3]);
-const jobCount = Number(process.argv[4]);
-const isMain = process.argv[5] === 'refs/heads/main';
-// const baseSha = isMain ? 'origin/main~1' : 'origin/main';
+try {
+  const target = process.argv[2];
+  const jobIndex = Number(process.argv[3]);
+  const jobCount = Number(process.argv[4]);
+  // const isMain = process.argv[5] === 'refs/heads/main';
+  // const baseSha = isMain ? 'origin/main~1' : 'origin/main';
 
-const affected = execSync(
-  `npx nx print-affected --base=${process.env.NX_BASE} --target=${process.env.NX_HEAD}`
-).toString('utf-8');
-const array = JSON.parse(affected)
-  .tasks.map((t) => t.target.project)
-  .slice()
-  .sort();
+  const affected = execSync(`npx nx print-affected`).toString('utf-8');
+  const array = JSON.parse(affected)
+    .tasks.map((t) => t.target.project)
+    .slice()
+    .sort();
 
-const sliceSize = Math.max(Math.floor(array.length / jobCount), 1);
+  console.log('Affected?', affected);
 
-const projects =
-  jobIndex < jobCount
-    ? array.slice(sliceSize * (jobIndex - 1), sliceSize * jobIndex)
-    : array.slice(sliceSize * (jobIndex - 1));
+  const sliceSize = Math.max(Math.floor(array.length / jobCount), 1);
 
-if (projects.length > 0) {
-  execSync(
-    `npx nx run-many --target=${target} --projects=${projects.join(
-      ','
-    )} --parallel ${restArgs()}`,
-    {
-      stdio: [0, 1, 2],
-    }
-  );
+  const projects =
+    jobIndex < jobCount
+      ? array.slice(sliceSize * (jobIndex - 1), sliceSize * jobIndex)
+      : array.slice(sliceSize * (jobIndex - 1));
+
+  if (projects.length > 0) {
+    execSync(
+      `npx nx run-many --target=${target} --projects=${projects.join(
+        ','
+      )} --parallel ${restArgs()}`,
+      {
+        stdio: [0, 1, 2],
+      }
+    );
+  }
+} catch (err) {
+  console.error('[run-many error]: ', err);
 }
 
 function restArgs() {
