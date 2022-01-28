f5a41468dda1a2230ac29b1ffb86e88a4e18739c
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2022-01-28 18:54:22 -0500

    Try without configs

--- karma.all.conf.js
+++ karma.all.conf.js
@@ -9,44 +9,47 @@ module.exports = function (config) {
       ...baseConfig.coverageReporter,
       dir: require('path').join(process.cwd(), './coverage/all'),
     },
-    browsers: ['ChromeHeadlessCustom', 'FirefoxHeadlessCustom'],
+    browsers: ['ChromeHeadless', 'FirefoxHeadless'],
     reporters: ['dots'],
     autoWatch: false,
     restartOnFileChange: false,
     browserDisconnectTimeout: 60000,
     browserNoActivityTimeout: 30000,
     captureTimeout: 60000,
-    customLaunchers: {
-      ChromeHeadlessCustom: {
-        base: 'ChromeHeadless',
-        flags: [
-          '--no-sandbox',
-          '--headless',
-          '--disable-gpu',
-          '--disable-translate',
-          '--disable-extensions',
-          '--disable-web-security',
-          '--disable-site-isolation-trials',
-        ],
-      },
-      FirefoxHeadlessCustom: {
-        base: 'FirefoxHeadless',
-        flags: ['--headless'],
-        prefs: {
-          'media.navigator.permission.disabled': true,
-          'network.proxy.type': 0,
-          'toolkit.telemetry.reportingpolicy.firstRun': false,
-          'extensions.enabledScopes': 0,
-          'app.update.disabledForTesting': true,
-        },
-      },
-    },
+    // customLaunchers: {
+    //   ChromeHeadlessCustom: {
+    //     base: 'ChromeHeadless',
+    //     flags: [
+    //       '--no-sandbox',
+    //       '--headless',
+    //       '--disable-gpu',
+    //       '--disable-translate',
+    //       '--disable-extensions',
+    //       '--disable-web-security',
+    //       '--disable-site-isolation-trials',
+    //     ],
+    //   },
+    //   FirefoxHeadlessCustom: {
+    //     base: 'FirefoxHeadless',
+    //     flags: ['--headless'],
+    //     prefs: {
+    //       'media.navigator.permission.disabled': true,
+    //       'network.proxy.type': 0,
+    //       'toolkit.telemetry.reportingpolicy.firstRun': false,
+    //       'extensions.enabledScopes': 0,
+    //       'app.update.disabledForTesting': true,
+    //     },
+    //   },
+    // },
   });
 
   // Tell karma to wait for bundle to be completed before launching browsers.
   // See: https://github.com/karma-runner/karma-chrome-launcher/issues/154#issuecomment-986661937
   config.plugins.unshift(require('./karma.waitwebpack'));
   config.frameworks.unshift('waitwebpack');
+
+  // TODO: clean up configs.
+  // try to go back to one karma config with both local and browserstack
 
   config.client.captureConsole = false;
 };
