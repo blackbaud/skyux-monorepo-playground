e437f5f34d53adcbbcfb2f9574e5ac71d0e3d19b
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2022-01-28 16:29:33 -0500

    Fix karma all

--- karma.all.conf.js
+++ karma.all.conf.js
@@ -9,11 +9,23 @@ module.exports = function (config) {
       ...baseConfig.coverageReporter,
       dir: require('path').join(process.cwd(), './coverage/all'),
     },
-    browsers: ['ChromeHeadless', 'FirefoxHeadless'],
+    browsers: ['ChromeHeadlessCustom', 'FirefoxHeadless'],
     reporters: ['dots'],
     browserDisconnectTimeout: 60000,
     browserNoActivityTimeout: 30000,
     captureTimeout: 60000,
+    customLaunchers: {
+      ChromeHeadlessCustom: {
+        base: 'ChromeHeadless',
+        flags: [
+          '--no-sandbox',
+          '--headless',
+          '--disable-gpu',
+          '--disable-translate',
+          '--disable-extensions',
+        ],
+      },
+    },
   });
 
   config.client.captureConsole = false;
