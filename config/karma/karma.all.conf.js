779b86e26c5f0dcf109215a85d5aa14f31c0b46e
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2022-01-31 09:05:40 -0500

    Try shared config

--- config/karma/karma.all.conf.js
+++ config/karma/karma.all.conf.js
@@ -1,26 +1,12 @@
-const getBaseKarmaConfig = require('../../karma.conf');
+const applySharedKarmaConfig = require('./karma.shared.conf');
 
 module.exports = function (config) {
-  const baseConfig = getBaseKarmaConfig();
+  applySharedKarmaConfig(config);
 
   config.set({
-    ...baseConfig,
-    client: {
-      ...baseConfig.client,
-      captureConsole: false,
-    },
     coverageReporter: {
       ...baseConfig.coverageReporter,
       dir: require('path').join(process.cwd(), './coverage/all'),
     },
-    browsers: ['ChromeHeadless'],
-    reporters: ['dots'],
-    autoWatch: false,
-    restartOnFileChange: false,
   });
-
-  // Tell karma to wait for bundle to be completed before launching browsers.
-  // See: https://github.com/karma-runner/karma-chrome-launcher/issues/154#issuecomment-986661937
-  config.plugins.unshift(require('./plugins/karma.waitwebpack'));
-  config.frameworks.unshift('waitwebpack');
 };
--- config/karma/karma.ci.conf.js
+++ config/karma/karma.ci.conf.js
@@ -1,88 +1,15 @@
-const getBaseKarmaConfig = require('../../karma.conf');
+const getBrowserStackLaunchers = require('./utils/browsers');
 
-function getBrowserSet(key) {
-  const bsBrowserEdge = {
-    base: 'BrowserStack',
-    browser: 'Edge',
-    os: 'Windows',
-    os_version: '10',
-  };
-
-  const bsBrowserFirefox = {
-    base: 'BrowserStack',
-    browser: 'Firefox',
-    browser_version: '95',
-    os: 'OS X',
-    os_version: 'Monterey',
-  };
-
-  const bsBrowserSafari = {
-    base: 'BrowserStack',
-    browser: 'Safari',
-    os: 'OS X',
-    os_version: 'Monterey',
-  };
-
-  const browserSets = {
-    speedy: [bsBrowserChrome],
-    quirky: [bsBrowserChrome, bsBrowserEdge],
-    paranoid: [bsBrowserEdge, bsBrowserFirefox, bsBrowserSafari],
-  };
-
-  return browserSets[key];
-}
-
-function getBrowserStackLaunchers(browserSetKey) {
-  const browserSet = getBrowserSet(browserSetKey);
-
-  const launchers = {};
-  for (const browser of browserSet) {
-    // Generate a key based on the browser information.
-    const key = [
-      browser.os || 'osDefault',
-      browser.os_version || 'osVersionDefault',
-      browser.browser || 'browserDefault',
-      browser.browser_version || 'browserVersionDefault',
-    ].join('_');
-
-    launchers[key] = browser;
-  }
-
-  return launchers;
-}
-
-function logBrowserStackSession(session) {
-  console.log(`
-
-  ****************************************************************************************************
-  Visit the following URL to view your BrowserStack results:
-  https://app.blackbaud.com/browserstack/sessions/${session}
-  ****************************************************************************************************
-
-  `);
-}
+const applySharedKarmaConfig = require('./karma.shared.conf');
 
 module.exports = function (config) {
-  const baseConfig = getBaseKarmaConfig();
-
-  config.set({
-    ...baseConfig,
-    client: {
-      ...baseConfig.client,
-      captureConsole: false,
-    },
-    reporters: ['dots'],
-    autoWatch: false,
-    browsers: ['ChromeHeadless'], // Fallback browser in case env variables not set.
-    singleRun: true,
-    restartOnFileChange: false,
-  });
+  applySharedKarmaConfig(config);
 
   delete config.coverageReporter;
 
   if (process.env.BROWSER_STACK_ACCESS_KEY) {
     const tunnelIdentifier = `browserstack_tunnel_${new Date().getTime()}`;
-    const customLaunchers = getBrowserStackLaunchers('paranoid');
+    const customLaunchers = getBrowserStackLaunchers();
 
     config.set({
       customLaunchers,
@@ -116,14 +43,16 @@ module.exports = function (config) {
         'type',
         function (/* BrowserStack:sessionMapping */ sessions) {
           this.onBrowserComplete = (browser) =>
-            logBrowserStackSession(sessions[browser.id]);
+            console.log(`
+
+  ****************************************************************************************************
+   Visit the following URL to view your BrowserStack results:
+   https://app.blackbaud.com/browserstack/sessions/${sessions[browser.id]}
+  ****************************************************************************************************
+
+  `);
         },
       ],
     });
-
-    // Tell karma to wait for bundle to be completed before launching browsers.
-    // See: https://github.com/karma-runner/karma-chrome-launcher/issues/154#issuecomment-986661937
-    config.plugins.unshift(require('./plugins/karma.waitwebpack'));
-    config.frameworks.unshift('waitwebpack');
   }
 };
