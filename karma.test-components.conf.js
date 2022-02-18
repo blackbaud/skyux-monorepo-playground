c35adae035dd4f5bd8479b2a2c2e92f6fa9b9b47
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2022-02-18 10:29:38 -0500

    Modify karma config

--- karma.test-components.conf.js
+++ karma.test-components.conf.js
@@ -8,15 +8,20 @@ module.exports = function (config) {
   const baseConfig = getBaseKarmaConfig();
   config.set({
     ...baseConfig,
-    // client: {
-    //   ...baseConfig.client,
-    //   captureConsole: false,
-    // },
+    client: {
+      ...baseConfig.client,
+      captureConsole: false,
+    },
     coverageReporter: {
       ...baseConfig.coverageReporter,
       dir: join(__dirname, './coverage/test-components'),
     },
-    // reporters: ['dots'],
+    reporters: ['dots'],
     browsers: ['ChromeHeadless'],
   });
+
+  // Tell karma to wait for bundle to be completed before launching browsers.
+  // See: https://github.com/karma-runner/karma-chrome-launcher/issues/154#issuecomment-986661937
+  config.plugins.unshift(require('./config/karma/plugins/karma.waitwebpack'));
+  config.frameworks.unshift('waitwebpack');
 };
