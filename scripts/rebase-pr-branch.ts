0b325fb5e7f06c4b700554da438af0082dc62479
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2022-02-22 10:43:40 -0500

    try again

--- scripts/rebase-pr-branch.ts
+++ scripts/rebase-pr-branch.ts
@@ -2,7 +2,7 @@ import { runCommand } from './utils/spawn';
 
 async function rebasePrBranch() {
   const baseRef = process.env.GITHUB_BASE_REF || 'main';
-  await runCommand('git', ['rebase', 'origin', baseRef]);
+  await runCommand('git', ['rebase', baseRef]);
 }
 
 rebasePrBranch();
