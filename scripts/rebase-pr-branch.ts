e8a1628cd572d90449e06ddb9467159eabda6108
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2022-02-22 11:07:47 -0500

    try something

--- scripts/rebase-pr-branch.ts
+++ scripts/rebase-pr-branch.ts
@@ -2,12 +2,21 @@ import { runCommand } from './utils/spawn';
 
 async function rebasePrBranch() {
   const baseRef = process.env.GITHUB_BASE_REF || 'main';
-  await runCommand('git', ['branch', '--show-current']);
+  // git config --global user.email "you@example.com"
+  // git config --global user.name "Your Name"
   await runCommand('git', [
-    'rebase',
-    `origin/${baseRef}`,
-    '--allow-empty-message',
+    'config',
+    '--global',
+    'user.email',
+    `"${process.env.GITHUB_ACTOR}"`,
   ]);
+  await runCommand('git', [
+    'config',
+    '--global',
+    'user.name',
+    `"${process.env.GITHUB_ACTOR}"`,
+  ]);
+  await runCommand('git', ['rebase', `origin/${baseRef}`]);
 }
 
 rebasePrBranch();
